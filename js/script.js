57a267d41026c8c1167fc862ac4c9732f75751aa
danielevergani <dvergani94@gmail.com>
2021-03-18 16:13:25 +0100

    inserito elemento con input e keypress

--- js/script.js
+++ js/script.js
@@ -15,3 +15,17 @@ for( var i = 0; i < listaSpesa.length; i++ ){
 $(".lista").on("click", ".delete__item", function(){
     $(this).parent().remove();
 });
+
+// inserisco elemento da input
+
+$("#ins__item").keypress(function(e){
+    if ( e.which == 13 ){
+        if ( $(this).val() != "" ){
+            var nItem =  $(this).val();
+            var template = $(".template li").clone();
+            template.prepend(nItem);
+            $(".lista").append(template); 
+            $(this).val("");
+        }
+    }
+});
