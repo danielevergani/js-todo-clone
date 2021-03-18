1c18c3e8edf65e2e80a6c98265c818f20305377a
danielevergani <dvergani94@gmail.com>
2021-03-18 15:46:07 +0100

    al click rimuovo oggetto usando remove (.on come evento)

--- js/script.js
+++ js/script.js
@@ -1,12 +1,17 @@
 var listaSpesa = ["uova", "latte", "burro"];
 
 
-// clono e assegno info al template
+// clono, assegno elementi array al template e "stampo"
 for( var i = 0; i < listaSpesa.length; i++ ){
 
     var template = $(".template li").clone();
     template.prepend(listaSpesa[i]);
     $(".lista").append(template);
-    
+
 }
 
+// cancello elementi al click su x usando .on
+
+$(".lista").on("click", ".delete__item", function(){
+    $(this).parent().remove();
+});
